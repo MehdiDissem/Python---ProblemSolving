021b8218c1dd0311da7f7c6150e952e46f7dfd5b
Mehdi Dissem <mehdissem@gmail.com>
2023-03-03 21:23:13 +0100

    range

---
+++ rangeExtrac.py
@@ -0,0 +1,27 @@
+# A format for expressing an ordered list of integers is to use a comma separated list of either
+
+# individual integers
+# or a range of integers denoted by the starting integer separated from the end integer in the range by a dash, '-'. The range includes all integers in the interval including both endpoints. It is not considered a range unless it spans at least 3 numbers. For example "12,13,15-17"
+# Complete the solution so that it takes a list of integers in increasing order and returns a correctly formatted string in the range format.
+
+# Example:
+
+# solution([-10, -9, -8, -6, -3, -2, -1, 0, 1, 3, 4, 5, 7, 8, 9, 10, 11, 14, 15, 17, 18, 19, 20])
+# # returns "-10--8,-6,-3-1,3-5,7-11,14,15,17-20"
+
+def solution(args):
+    result = ""
+
+    while len(args) > 0:
+        first = last = args.pop(0)
+        while (len(args) > 0) and (args[0] == last + 1):
+            last = args.pop(0)
+
+        if first == last:
+            result += f"{first},"
+        elif last - first == 1:
+            result += f"{first},{last},"
+        else:
+            result += f"{first}-{last},"
+    
+    return result[:-1]
